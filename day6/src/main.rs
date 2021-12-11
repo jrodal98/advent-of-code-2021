4f740d9ba729c2c84f6e6b1b915002973257a9b3
Jacob Rodal <jrodal@fb.com>
2021-12-11 01:51:27 -0800

    day 6 cleanup

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -12,14 +12,12 @@ fn solution(input: &str, num_days: u64) -> u64 {
         .trim()
         .split(',')
         .map(|s| s.parse().unwrap())
-        .fold(0, |acc, s| {
-            acc + get_num_children(s, num_days, &mut cache) + 1
-        })
+        .fold(0, |acc, s| acc + num_fish(s, num_days, &mut cache))
 }
 
-fn get_num_children(state: u64, days_remaining: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
+fn num_fish(state: u64, days_remaining: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
     if days_remaining == 0 {
-        return 0;
+        return 1;
     }
     let cache_key = &(state, days_remaining);
     if let Some(cached_val) = cache.get(cache_key) {
@@ -27,10 +25,9 @@ fn get_num_children(state: u64, days_remaining: u64, cache: &mut HashMap<(u64, u
     }
 
     let num_children = if state == 0 {
-        1 + get_num_children(6, days_remaining - 1, cache)
-            + get_num_children(8, days_remaining - 1, cache)
+        num_fish(6, days_remaining - 1, cache) + num_fish(8, days_remaining - 1, cache)
     } else {
-        get_num_children(state - 1, days_remaining - 1, cache)
+        num_fish(state - 1, days_remaining - 1, cache)
     };
 
     if cache.insert(*cache_key, num_children).is_some() {
