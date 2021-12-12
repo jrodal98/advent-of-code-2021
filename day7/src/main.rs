6aa5a7412cb0791559cad8f994650d30c99bc66b
Jacob Rodal <jrodal@fb.com>
2021-12-11 16:39:20 -0800

    day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     );
 }
 
-fn solution(input: &str, fuel_consumption_func: fn(i32, i32) -> i32) -> i32 {
+fn solution(input: &str, consume_fuel: fn(i32, i32) -> i32) -> i32 {
     let positions: Vec<i32> = input
         .trim()
         .split(',')
@@ -25,9 +25,9 @@ fn solution(input: &str, fuel_consumption_func: fn(i32, i32) -> i32) -> i32 {
 
     (min_pos..=max_pos)
         .map(|target_pos| {
-            positions.iter().fold(0, |acc, &crab_pos| {
-                acc + fuel_consumption_func(crab_pos, target_pos)
-            })
+            positions
+                .iter()
+                .fold(0, |acc, &crab_pos| acc + consume_fuel(crab_pos, target_pos))
         })
         .min()
         .unwrap()
@@ -44,7 +44,7 @@ fn test_problem1() {
 fn test_problem2() {
     let input = include_str!("../data/sample.txt");
     let res = solution(input, |crab_pos, target_pos| {
-        (((crab_pos - target_pos).abs() * ((crab_pos - target_pos).abs() + 1)) / 2)
+        ((crab_pos - target_pos).abs() * ((crab_pos - target_pos).abs() + 1)) / 2
     });
     assert_eq!(res, 168);
 }
