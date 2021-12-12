2f61b13b88a49e13a1a9c5835249e3b2c3152f74
Jacob Rodal <jrodal@fb.com>
2021-12-12 00:23:42 -0800

    day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
     let input = include_str!("../data/input.txt");
     println!("Problem 1: {}", problem1(input));
@@ -9,7 +7,7 @@ fn main() {
 fn problem1(input: &str) -> usize {
     input
         .lines()
-        .flat_map(|line| Entry::new(line).output_values)
+        .flat_map(|line| Solver::new(line).output_values)
         .filter(|output_vec| match output_vec.len() {
             2 | 3 | 4 | 7 => true,
             _ => false,
@@ -18,16 +16,77 @@ fn problem1(input: &str) -> usize {
 }
 
 fn problem2(input: &str) -> u32 {
-    0
+    input
+        .lines()
+        .map(|line| Solver::new(line).compute_output_val())
+        .sum()
 }
 
-#[derive(Hash, Eq, PartialEq)]
-struct Entry {
-    signal_patterns: Vec<Vec<char>>,
+struct Translator {
+    one_chars: Vec<char>,
+    four_chars: Vec<char>,
+}
+
+impl Translator {
+    fn new(jumbled_wires: &Vec<Vec<char>>) -> Self {
+        let one_chars = jumbled_wires
+            .iter()
+            .find(|wire| wire.len() == 2)
+            .unwrap()
+            .to_vec();
+        let four_chars = jumbled_wires
+            .iter()
+            .find(|wire| wire.len() == 4)
+            .unwrap()
+            .to_vec();
+
+        Self {
+            one_chars,
+            four_chars,
+        }
+    }
+
+    fn num_common_with_one(&self, jumbled_wire: &Vec<char>) -> u8 {
+        Self::num_common_segments(&self.one_chars, jumbled_wire)
+    }
+
+    fn num_common_with_four(&self, jumbled_wire: &Vec<char>) -> u8 {
+        Self::num_common_segments(&self.four_chars, jumbled_wire)
+    }
+
+    fn num_common_segments(wire1: &Vec<char>, wire2: &Vec<char>) -> u8 {
+        wire1
+            .iter()
+            .fold(0, |acc, c| if wire2.contains(c) { acc + 1 } else { acc })
+    }
+
+    fn translate(&self, jumbled_wire: &Vec<char>) -> char {
+        match (
+            jumbled_wire.len(),
+            self.num_common_with_one(jumbled_wire),
+            self.num_common_with_four(jumbled_wire),
+        ) {
+            (2, _, _) => '1',
+            (5, _, 2) => '2',
+            (5, 2, _) => '3',
+            (4, _, _) => '4',
+            (5, 1, _) => '5',
+            (6, 1, _) => '6',
+            (3, _, _) => '7',
+            (7, _, _) => '8',
+            (6, _, 4) => '9',
+            (6, _, 3) => '0',
+            (_, _, _) => unreachable!(),
+        }
+    }
+}
+
+struct Solver {
     output_values: Vec<Vec<char>>,
+    translator: Translator,
 }
 
-impl Entry {
+impl Solver {
     fn new(input: &str) -> Self {
         let mut sections = input
             .split(" | ")
@@ -39,10 +98,23 @@ impl Entry {
             })
             .collect::<Vec<_>>();
 
+        let output_values = sections.pop().unwrap();
+        let signal_patterns = sections.pop().unwrap();
+        let translator = Translator::new(&signal_patterns);
+
         Self {
-            output_values: sections.pop().unwrap(),
-            signal_patterns: sections.pop().unwrap(),
+            output_values,
+            translator,
         }
+    }
+
+    fn compute_output_val(&self) -> u32 {
+        self.output_values
+            .iter()
+            .map(|wire| self.translator.translate(wire))
+            .collect::<String>()
+            .parse::<u32>()
+            .unwrap()
     }
 }
 
@@ -57,5 +129,5 @@ fn test_problem1() {
 fn test_problem2() {
     let input = include_str!("../data/sample.txt");
     let res = problem2(input);
-    assert_eq!(res, 0);
+    assert_eq!(res, 61229);
 }
