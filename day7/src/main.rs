366df5b2ca21e1dc5487df1eb9808dfb7aefb9f6
Jacob Rodal <jrodal@fb.com>
2021-12-11 17:07:06 -0800

    day 7 use trait instead of closure

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,50 +1,58 @@
 fn main() {
     let input = include_str!("../data/input.txt");
-    println!(
-        "Problem 1: {}",
-        solution(input, |crab_pos, target_pos| (crab_pos - target_pos).abs())
-    );
-    println!(
-        "Problem 2: {}",
-        solution(input, |crab_pos, target_pos| (((crab_pos - target_pos)
-            .abs()
-            * ((crab_pos - target_pos).abs() + 1))
-            / 2))
-    );
+    println!("Problem 1: {}", Problem1::compute_min_fuel_use(input));
+    println!("Problem 2: {}", Problem2::compute_min_fuel_use(input));
 }
 
-fn solution(input: &str, consume_fuel: fn(i32, i32) -> i32) -> i32 {
-    let positions: Vec<i32> = input
-        .trim()
-        .split(',')
-        .map(|c| c.parse().unwrap())
-        .collect();
+struct Problem1;
+struct Problem2;
 
-    let &min_pos = positions.iter().min().unwrap();
-    let &max_pos = positions.iter().max().unwrap();
+trait CrabSubmarine {
+    fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32;
 
-    (min_pos..=max_pos)
-        .map(|target_pos| {
-            positions
-                .iter()
-                .fold(0, |acc, &crab_pos| acc + consume_fuel(crab_pos, target_pos))
-        })
-        .min()
-        .unwrap()
+    fn compute_min_fuel_use(input: &str) -> i32 {
+        let positions: Vec<i32> = input
+            .trim()
+            .split(',')
+            .map(|c| c.parse().unwrap())
+            .collect();
+
+        let &min_pos = positions.iter().min().unwrap();
+        let &max_pos = positions.iter().max().unwrap();
+
+        (min_pos..=max_pos)
+            .map(|target_pos| {
+                positions.iter().fold(0, |acc, &crab_pos| {
+                    acc + Self::consume_fuel(crab_pos, target_pos)
+                })
+            })
+            .min()
+            .unwrap()
+    }
+}
+
+impl CrabSubmarine for Problem1 {
+    fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32 {
+        (crab_pos - target_pos).abs()
+    }
+}
+
+impl CrabSubmarine for Problem2 {
+    fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32 {
+        ((crab_pos - target_pos).abs() * ((crab_pos - target_pos).abs() + 1)) / 2
+    }
 }
 
 #[test]
 fn test_problem1() {
     let input = include_str!("../data/sample.txt");
-    let res = solution(input, |crab_pos, target_pos| (crab_pos - target_pos).abs());
+    let res = Problem1::compute_min_fuel_use(input);
     assert_eq!(res, 37);
 }
 
 #[test]
 fn test_problem2() {
     let input = include_str!("../data/sample.txt");
-    let res = solution(input, |crab_pos, target_pos| {
-        ((crab_pos - target_pos).abs() * ((crab_pos - target_pos).abs() + 1)) / 2
-    });
+    let res = Problem2::compute_min_fuel_use(input);
     assert_eq!(res, 168);
 }
