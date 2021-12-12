4847f8d5a8f9dd15b1a3caf1bdfebdf7dd4e057b
Jacob Rodal <jrodal@fb.com>
2021-12-11 17:31:39 -0800

    day 7 use trait instead of closure

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let input = include_str!("../data/input.txt");
     let crab_data = &CrabData::new(input);
-    println!("Problem 1: {}", Problem1::compute_min_fuel_use(crab_data));
-    println!("Problem 2: {}", Problem2::compute_min_fuel_use(crab_data));
+    let problem1 = Problem1 { crab_data };
+    let problem2 = Problem2 { crab_data };
+    println!("Problem 1: {}", problem1.compute_min_fuel_use());
+    println!("Problem 2: {}", problem2.compute_min_fuel_use());
 }
-
-struct Problem1;
-struct Problem2;
 
 struct CrabData {
     crab_positions: Vec<i32>,
@@ -35,8 +34,11 @@ impl CrabData {
 
 trait CrabSubmarine {
     fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32;
+    fn get_crab_data(&self) -> &CrabData;
 
-    fn compute_min_fuel_use(crab_data: &CrabData) -> i32 {
+    fn compute_min_fuel_use(&self) -> i32 {
+        let crab_data = self.get_crab_data();
+
         (crab_data.min_crab..=crab_data.max_crab)
             .map(|target_pos| {
                 crab_data.crab_positions.iter().fold(0, |acc, &crab_pos| {
@@ -48,15 +50,30 @@ trait CrabSubmarine {
     }
 }
 
-impl CrabSubmarine for Problem1 {
+struct Problem1<'a> {
+    crab_data: &'a CrabData,
+}
+
+impl CrabSubmarine for Problem1<'_> {
     fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32 {
         (crab_pos - target_pos).abs()
     }
+
+    fn get_crab_data(&self) -> &CrabData {
+        self.crab_data
+    }
 }
 
-impl CrabSubmarine for Problem2 {
+struct Problem2<'a> {
+    crab_data: &'a CrabData,
+}
+
+impl CrabSubmarine for Problem2<'_> {
     fn consume_fuel(crab_pos: i32, target_pos: i32) -> i32 {
         ((crab_pos - target_pos).abs() * ((crab_pos - target_pos).abs() + 1)) / 2
+    }
+    fn get_crab_data(&self) -> &CrabData {
+        self.crab_data
     }
 }
 
@@ -64,7 +81,8 @@ impl CrabSubmarine for Problem2 {
 fn test_problem1() {
     let input = include_str!("../data/sample.txt");
     let crab_data = &CrabData::new(input);
-    let res = Problem1::compute_min_fuel_use(crab_data);
+    let problem1 = Problem1 { crab_data };
+    let res = problem1.compute_min_fuel_use();
     assert_eq!(res, 37);
 }
 
@@ -72,6 +90,7 @@ fn test_problem1() {
 fn test_problem2() {
     let input = include_str!("../data/sample.txt");
     let crab_data = &CrabData::new(input);
-    let res = Problem2::compute_min_fuel_use(crab_data);
+    let problem2 = Problem2 { crab_data };
+    let res = problem2.compute_min_fuel_use();
     assert_eq!(res, 168);
 }
